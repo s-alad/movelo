4d4a77518102750b00b8dd94a56efa8174666cb2
Collin Barber <collinjbarber@gmail.com>
2024-02-22 17:38:42 -0500

    clear

--- old/movelo-app/types/env.d.ts
+++
@@ -1,3 +0,0 @@
-declare module '@env' {
-    export const GOOGLE_MAPS_API_KEY: string;
-}
--- old/movelo-site/components/divider/divider.tsx
+++
@@ -1,7 +0,0 @@
-import s from './divider.module.scss'
-
-export default function Divider() {
-    return (
-        <div className={s.divider}><div className={s.divide}></div></div>
-    )
-}
